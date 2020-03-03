439c149fca4b0a9d2e30a731859d14bd6abefa3f
jinyue <983656956@qq.com>
2020-03-03 14:41:28 +0800

    1,新增thread.join的demo

---
+++ general-java/src/main/java/com/jinyue/executeflow/VoidMethodReturnTest.java
@@ -0,0 +1,13 @@
+package com.jinyue.executeflow;
+
+public class VoidMethodReturnTest {
+    public static void main(String[] args) {
+        // 注意：void method 执行return语句后，main线程不会结束，还会继续执行，因此下面的语句是可以打印的
+        testVoidMethodReturn();
+        System.out.println("void method return successfully");
+    }
+
+    private static void testVoidMethodReturn() {
+        return;
+    }
+}
