b354a09052a6bc8376e6219c48ade88d253cc1ba
jinyue <983656956@qq.com>
2020-03-08 21:54:54 +0800

    1,新增springboot2的demo

---
+++ springboot2-demo/src/main/java/com/ymbj/helloworld/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.ymbj.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class HelloWorldController {
+    @ResponseBody
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
